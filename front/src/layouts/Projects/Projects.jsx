de923d8d6e3858a8ba1f69db369385b61dab0460
Oleg0110 <oleg.buno13@gmail.com>
2021-08-18 18:51:39 +0300

    add search projects

--- front/src/layouts/Projects/Projects.jsx
+++ front/src/layouts/Projects/Projects.jsx
@@ -7,27 +7,63 @@ import { observer } from "mobx-react"
 
 const Projects = ({ children, onClick }) => {
 
+   const buttonNamecreate = "Create Project"
+   const buttonNamehide = "Hide Block"
+
    const { projects } = ProjectsStore
 
+   const searchTitleref = useRef(null)
    const nameRef = useRef(null)
    const descriptionRef = useRef(null)
    const [isOpened, setIsOpened] = useState(false)
-   const [isChangeName, setChangeName] = useState("Create Project")
+   const [isChangeName, setChangeName] = useState(buttonNamecreate)
+   const [searchTitle, setSearchTitle] = useState('')
+   const [searchContent, setSearchContent] = useState('')
+   const [isSearchOpened, setIsSearchOpened] = useState(false)
+   // const [deleteSearch, setDeleteSearch] = useState(searchTitle)
+
+
+   const foundTitle = projects.filter(found => found.title.toLowerCase().includes(searchTitle.toLowerCase()))
+   const foundContent = projects.filter(found => found.content.toLowerCase().includes(searchContent.toLowerCase()))
 
 
    return (
       <div className={styles.mainProjectsStyle}>
          <h1 className={styles.mainTitle}>Projects</h1>
          <div className={styles.create}>
-            <Button ><span className={styles.filtersButton}>Filters<div className={styles.chevronIcon} alt="Arrow" /></span></Button>
+            <div className={styles.searchBlock}>
+               <Button ><div className={styles.filtersButton}>Filters<div className={styles.chevronIcon} alt="Arrow" /></div></Button>
+               <div className={styles.searckArea}>
+                  <form className={styles.titleSearch} onChange={(e) => {
+                     e.preventDefault()
+                     setSearchTitle(searchTitleref.current.value)
+                  }}>
+                     <input type="text" placeholder="Title search..." className={styles.searchInput} ref={searchTitleref} />
+                     <div className={styles.searchIcon} alt="Search Icon" />
+                     <Button><div className={styles.delete} alt="Delete Icon" /></Button>
+                  </form>
+                  <Button onClick={() => setIsSearchOpened(!isSearchOpened)}><div className={styles["three-dots"]} alt="Dots" /></Button>
+                  <form className={`${styles.contentSearch} ${isSearchOpened && styles.contentSearchOpened}`} onChange={(e) => {
+                     e.preventDefault()
+                     setSearchTitle(searchTitleref.current.value)
+                  }}>
+                     <input type="text" placeholder="Content search..." className={styles.searchInput}
+                        onChange={(e) => setSearchContent(e.target.value)} />
+                     <div className={styles.searchIcon} alt="Search Icon" />
+                     <Button><div className={styles.delete} alt="Delete Icon" /></Button>
+                  </form>
+               </div>
+            </div>
             <Button onClick={() => {
                setIsOpened(!isOpened)
-               setChangeName(isChangeName === "Create Project" ? "Hide Block" : "Create Project")
+               setChangeName(isChangeName === buttonNamecreate ? buttonNamehide : buttonNamecreate)
             }} buttonStyle="thirdButtonStyle">{isChangeName}</Button>
          </div>
          <div className={styles.projectsBlocks}>
             <div className={`${styles.accordionBlock} ${isOpened && styles.accordionBlock || styles.accordionBlockMax} `} >
-               {projects.map((data) => <Accordion title={data.title} content={data.content} key={data.id} />)}
+               {foundTitle.map((data) =>
+                  <Accordion title={data.title} content={data.content} key={data.id} />
+               )}
             </div>
             <div className={`${styles.createProjectArea} ${isOpened && styles.openedCreateProjectBlock || styles.closeCreateProjectBlock} `}>
                <div className={styles.createArea}>
